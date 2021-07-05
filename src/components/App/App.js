336a798d2315f72c10971710d41524303a92bf84
Kai Mareving <karska.joanna@gmail.com>
2021-07-05 16:31:11 +0200

    15.3: Change App/List/Hero v.0.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './App.scss';
+import List from '../List/List.js';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+      <main className={styles.component}>
+        <h1 className={styles.title}>React & Webpack</h1>
+        <h2 className={styles.subtitle}>TO-DO List</h2>
+        <List />
+      </main>
     )
   }
 }
