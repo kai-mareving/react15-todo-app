000b5d958b05ed9965d425869593b78d41639f2a
Kai Mareving <karska.joanna@gmail.com>
2021-07-10 14:47:20 +0200

    Add react-confirm-alert v.0.1

--- src/components/Creator/Creator.js
+++ src/components/Creator/Creator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Creator.scss';
 import Button from '../Button/Button';
 import PropTypes from 'prop-types';
+import { confirmAlert } from 'react-confirm-alert';
 
 class Creator extends React.Component {
   static propTypes = {
@@ -36,12 +37,28 @@ class Creator extends React.Component {
   }
 
   handleCancel = () => {
-    if (window.confirm("Do you really want to cancel?")) {
-      this.setState({
-        value: '',
-        visibleButtons: false
-      });
-    }
+    confirmAlert({
+      customUI: ({ onClose }) => {
+        return (
+          <div className={styles.confirm}>
+            <div>
+            <h1>Are you sure?</h1>
+            <p>You want to delete this file?</p>
+            <Button onClick={onClose} variant="confirm">No</Button>
+            <Button
+              onClick={() => {
+                this.setState({ value: '', visibleButtons: false });
+                onClose();
+              }} variant='danger'>Yes, Delete it!
+            </Button>
+            </div>
+          </div>
+        );
+      }
+    });
+
+    /* if (window.confirm("Do you really want to cancel?")) {
+      this.setState({ value: '', visibleButtons: false }); } */
   }
 
   render() {
