ed29206093ed60e19089471d5a60a44c530d04a7
Kai Mareving <karska.joanna@gmail.com>
2021-07-18 15:39:56 +0200

    Add card filtering v.0.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from './App.scss';
 import PropTypes from 'prop-types';
 import List from '../List/ListContainer';
 // import Creator from '../Creator/Creator';
+import Search from '../Search/SearchContainer';
 
 class App extends React.Component {
   static propTypes = {
@@ -36,6 +37,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
 
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
